ad3269a61327044570a92678a7b6f0baabe1b9ff
Alfredo Motta <amotta@housetrip.com>
2015-12-26 10:48:26 +0000

    multinomial one-vs-all logistic regression

--- lib/logistic_regression.py
+++ lib/logistic_regression.py
@@ -15,24 +15,80 @@ class Binomial(LogisticRegression):
         self.theta = None
 
     def predict(self, X):
-        n, dim = X.shape
-        x = np.ones([n, dim+1])
-        x[: , 1: ] = X
+        x = self._add_ones_to(X)
 
         threshold = self._logistic(x.dot(self.theta))
-        return (threshold <= 0.5).astype(int)
+        return np.rint(threshold)
 
     def fit(self, X, y):
         n, dim = X.shape
 
         self.theta = np.zeros(dim + 1)
-        x = np.ones([n, dim+1])
-        x[:, 1: ] = X
+        x = self._add_ones_to(X)
 
         for _ in range(self.iterations):
-            self.theta -= self.alpha * (y - self._logistic(x.dot(self.theta))).dot(x)
+            self.theta += self.alpha * x.T.dot(y - self._logistic(x.dot(self.theta)))
 
         return self
+
+    def _logistic(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def _add_ones_to(self, X):
+        n, dim = X.shape
+        x = np.ones([n, dim+1])
+        x[: , 1: ] = X
+
+        return x
+
+
+class Multinomial(LogisticRegression):
+    def __init__(self, options={'iterations': 1000, 'alpha': 0.01}):
+        self.regressors = []
+        self.options = options
+
+    def predict(self, X):
+        x = self._add_ones_to(X)
+
+        probabilities = self._compute_probabilities(self.regressors, x)
+
+        return np.argmax(probabilities, axis=0)
+
+    def fit(self, X, y):
+        self.klasses = np.unique(y)
+
+        for k in self.klasses[0:-1]:
+            y_ = (y == k).astype(int)
+            self.regressors.append(self._get_regressor_for(X, y_))
+
+        return self
+
+    def _get_regressor_for(self, X, y):
+        regressor = Binomial(self.options)
+        regressor.fit(X, y)
+
+        return regressor
+
+    def _add_ones_to(self, X):
+        n, dim = X.shape
+        x = np.ones([n, dim+1])
+        x[: , 1: ] = X
+
+        return x
+
+    def _compute_probabilities(self, regressors, x):
+        n, dim = x.shape
+        numerators = np.array([]).reshape(0, n)
+        probabilities = np.array([]).reshape(0, n)
+
+        for regressor in self.regressors:
+            numerators = np.vstack([numerators, np.exp(x.dot(regressor.theta))])
+
+        for numerator in numerators:
+            probabilities = np.vstack([probabilities, numerator / (1 + np.sum(numerators, axis=0))])
+        probabilities = np.vstack([probabilities, 1 / (1 + np.sum(numerators, axis=0))])
+
+        return probabilities
 
     def _logistic(self, x):
         return 1 / (1 + np.exp(-x))
--- test/logistic_regression_test.py
+++ test/logistic_regression_test.py
@@ -6,35 +6,69 @@ from sklearn.linear_model import LogisticRegression
 from sklearn import datasets
 
 class TestLogisticRegression:
-    @pytest.fixture
-    def data(self):
-        return datasets.make_classification(500, n_features=1, n_informative=1, n_redundant=0, n_clusters_per_class=1)
-
-    @pytest.fixture
-    def x(self, data):
-        return data[0]
-
-    @pytest.fixture
-    def y(self, data):
-        return data[1]
-
-    class TestBinomial:
+    class TestTwoClasses:
         @pytest.fixture
-        def subject(self, x, y):
-            return Binomial()
+        def data(self):
+            return datasets.make_classification(500, n_features=1, n_informative=1, n_redundant=0, n_clusters_per_class=1, random_state=10)
 
-        def test_has_a_decent_accuracy(self, subject, x, y):
-            subject.fit(x, y)
-
-            assert len(np.where(subject.predict(x) == y)[0]) >= 400
-
-    class TestSkLearn:
         @pytest.fixture
-        def subject(self):
-            return LogisticRegression()
+        def x(self, data):
+            return data[0]
 
-        def test_has_a_decent_accuracy(self, subject, x, y):
-            subject.fit(x, y)
+        @pytest.fixture
+        def y(self, data):
+            return data[1]
 
-            assert len(np.where(subject.predict(x) == y)[0]) >= 400
+        class TestBinomial:
+            @pytest.fixture
+            def subject(self, x, y):
+                return Binomial()
 
+            def test_has_a_decent_accuracy(self, subject, x, y):
+                subject.fit(x, y)
+
+                assert np.sum(subject.predict(x) == y) >= 400
+
+        class TestSkLearn:
+            @pytest.fixture
+            def subject(self):
+                return LogisticRegression()
+
+            def test_has_a_decent_accuracy(self, subject, x, y):
+                subject.fit(x, y)
+
+                assert np.sum(subject.predict(x) == y) >= 400
+
+
+    class TestMultipleClasses:
+        @pytest.fixture
+        def data(self):
+            return datasets.make_classification(500, n_features=4, n_informative=4, n_classes=4, n_redundant=0, n_clusters_per_class=1)
+
+        @pytest.fixture
+        def x(self, data):
+            return data[0]
+
+        @pytest.fixture
+        def y(self, data):
+            return data[1]
+
+        class TestMultinomial:
+            @pytest.fixture
+            def subject(self, x, y):
+                return Multinomial()
+
+            def test_has_a_decent_accuracy(self, subject, x, y):
+                subject.fit(x, y)
+
+                assert np.sum(subject.predict(x) == y) >= 300
+
+        class TestSkLearn:
+            @pytest.fixture
+            def subject(self):
+                return LogisticRegression()
+
+            def test_has_a_decent_accuracy(self, subject, x, y):
+                subject.fit(x, y)
+
+                assert np.sum(subject.predict(x) == y) >= 400
